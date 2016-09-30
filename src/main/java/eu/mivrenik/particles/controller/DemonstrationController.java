823fa570b3322cb98f0a67ed0b04bc275128dd98
Michael Orlov <overdzen@gmail.com>
2016-10-01 00:53:08 +0300

    Add method's redrawBoltzmannDistribution implementation

--- src/main/java/eu/mivrenik/particles/controller/DemonstrationController.java
+++ src/main/java/eu/mivrenik/particles/controller/DemonstrationController.java
@@ -1,5 +1,19 @@
 package eu.mivrenik.particles.controller;
 
+import eu.mivrenik.particles.io.ExperimentLoader;
+import eu.mivrenik.particles.model.ExperimentState;
+import eu.mivrenik.particles.model.Particle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Spinner;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -7,18 +21,6 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
-
-import eu.mivrenik.particles.io.ExperimentLoader;
-import eu.mivrenik.particles.model.ExperimentState;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.LineChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.Spinner;
 
 /**
  * Demonstration scene controller.
@@ -71,7 +73,7 @@ public class DemonstrationController implements Initializable {
 
         redraw(state);
         redrawMaxwellDistribution(state);
-        redrawBoltzmannDistribution(state);
+        redrawBoltzmannDistribution(state, 20);
 
         if (updateSlider) {
             timeSlider.setValue((double) newValue);
@@ -86,8 +88,38 @@ public class DemonstrationController implements Initializable {
         // TODO Method stub.
     }
 
-    private void redrawBoltzmannDistribution(final ExperimentState state) {
-        // TODO Method stub.
+    private void redrawBoltzmannDistribution(final ExperimentState state, final int binsNum) {
+        double heightMax = Double.MIN_VALUE;
+        double heightMin = Double.MAX_VALUE;
+
+        for (Particle particle : state.getParticles()) {
+            heightMax = Math.max(heightMax, particle.getPosY());
+            heightMin = Math.min(heightMin, particle.getPosY());
+        }
+
+        double deltaHeight = heightMax / binsNum;
+        double[] x = new double[binsNum];
+        int[] y = new int[binsNum];
+
+        for (int i = 0; i < binsNum; i++) {
+            y[i] = 0;
+            x[i] = deltaHeight * (i + 1);
+        }
+
+        for (Particle particle : state.getParticles()) {
+            int chunk = (int) ((particle.getPosY() - deltaHeight / 2.0) / deltaHeight);
+            y[chunk]++;
+        }
+
+        XYChart.Series boltzmannSeries = new XYChart.Series();
+
+        for (int i = 0; i < binsNum; i++) {
+            boltzmannSeries.getData().add(new XYChart.Data(x[i], y[i]));
+        }
+
+        barChart.getData().add(boltzmannSeries);
+
+        // TODO Set bar width
     }
 
 
